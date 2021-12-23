a1353e688830b4e10162f0e6ef15edb9a730ab58
Jinkyu <xxyxy000@gmail.com>
2021-12-24 02:13:15 +0900

    2nd

--- main.js
+++ main.js
@@ -232,6 +232,6 @@ window.addEventListener('click', (e) => {
 
 
 function shareTwitter() {
-    const shareUrl = "devpad.tistory.com/"; // 전달할 URL
+    const shareUrl = "penis-grade.netlify.app/"; // 전달할 URL
     window.open(`https://twitter.com/intent/tweet?text=저의 페니스 등급은 ${gradeInfo[gradeInfoNo]['totalGrade']}입니다 ${gradeInfo[gradeInfoNo]['emoji']}&url=${shareUrl}`);
 }
